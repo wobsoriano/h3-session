a62a44090b81f8af4ec7ae0d6d3171ec5a400376
Robert Soriano <sorianorobertc@gmail.com>
2022-06-02 00:18:23 -0700

    refactor: run lint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import session, { SessionOptions } from 'express-session'
+import type { SessionOptions } from 'express-session'
+import session from 'express-session'
 import type { EventHandler } from 'h3'
 
 export function SessionHandler(options: SessionOptions): EventHandler<void> {
