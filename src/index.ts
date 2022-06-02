abe32507cf52fb533f481b9cf0e787e3832a3966
Robert Soriano <sorianorobertc@gmail.com>
2022-06-02 00:19:22 -0700

    feat: export express-session types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import type { SessionOptions } from 'express-session'
+import type { SessionOptions, Session } from 'express-session'
 import session from 'express-session'
 import type { EventHandler } from 'h3'
 
 export function SessionHandler(options: SessionOptions): EventHandler<void> {
   return session(options) as any
+}
+
+export type {
+  SessionOptions,
+  Session
 }
