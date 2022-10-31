184633c87b1883988a1f1cddb6af3dce33f9c98a
Robert Soriano <sorianorobertc@gmail.com>
2022-10-31 13:18:59 -0700

    add nuxt module support

---
+++ nuxt.mjs
@@ -0,0 +1,38 @@
+import { addServerHandler, addTemplate, createResolver, defineNuxtModule, useLogger } from '@nuxt/kit'
+
+export default defineNuxtModule({
+  meta: {
+    name: 'h3-session',
+    configKey: 'session',
+    compatibility: {
+      nuxt: '^3.0.0-rc.12',
+    },
+  },
+  setup(moduleOptions, nuxt) {
+    const logger = useLogger('h3-session')
+
+    const { resolve } = createResolver(import.meta.url)
+    nuxt.options.runtimeConfig.session = {
+      ...nuxt.options.runtimeConfig.session || {},
+      ...moduleOptions,
+    }
+
+    addTemplate({
+      filename: 'session-handler.mjs',
+      write: true,
+      getContents: () => `
+        import { createSessionHandler } from 'h3-session'
+        import { useRuntimeConfig } from '#imports'
+        console.log(useRuntimeConfig().session)
+        export default createSessionHandler(useRuntimeConfig().session)
+      `,
+    })
+
+    addServerHandler({
+      handler: resolve(nuxt.options.buildDir, 'session-handler.mjs'),
+      middleware: true,
+    })
+
+    logger.success('h3-session middleware installed')
+  },
+})
